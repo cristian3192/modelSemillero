81d6b362e7fdb4980a548117fad55c08a4ed8311
Cristian <cris_t_15@hotmail.com>
2020-12-10 09:56:41 -0500

    se agrego la edad al estudiante

--- src/main/java/com/clearminds/ccc/dtos/Estudiante.java
+++ src/main/java/com/clearminds/ccc/dtos/Estudiante.java
@@ -6,27 +6,32 @@ public class Estudiante  {
 	private String nombre;
 	private String apellido;
 	private int id;
+	private int edad;
 	
-	public Estudiante(String nombre, String apellido, int id) {
+	public Estudiante(String nombre, String apellido, int id, int edad) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.id = id;
+		this.edad = edad;
 	}
 
-	public Estudiante(String nombre, String apellido) {
+	public Estudiante(String nombre, String apellido, int edad) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.edad = edad;
 	}
 	
 	public Estudiante (){
 		
 	}
 
+	
+
 	@Override
 	public String toString() {
-		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + ", edad=" + edad + "]";
 	}
 
 	public String getNombre() {
@@ -51,6 +56,14 @@ public class Estudiante  {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
 	}
 	
 	
